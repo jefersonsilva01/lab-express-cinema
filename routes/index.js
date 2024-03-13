56e8fd4e96ecad3a4074f8e843ffdfe7ddb4603e
jefersonsilva01 <jeferson.s.silva1@gmail.com>
2024-03-13 15:37:24 -0300

    Done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('../models/Movie.model');
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then((movies) => {
+      console.log('Movies finded ', movies);
+      res.render('movies', { movies });
+    })
+    .catch((error) => {
+      console.log('An error occurred ', error);
+    });
+});
+
+router.get('/movie/:id', (req, res, next) => {
+  const movieId = req.params.id
+  Movie.findById(movieId)
+    .then((movie) => {
+      console.log('Movie finded', movie)
+      res.render('movie', movie);
+    })
+    .catch((error) => {
+      console.log('An error occurred', error);
+    })
+})
 
 module.exports = router;
